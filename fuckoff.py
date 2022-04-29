b0832f7480a8b5a61c782b53063f722d5158cc7f
Mass Owner <101129113+Mass-Action@users.noreply.github.com>
2022-04-29 21:48:33 +0530

    Update fuckoff.py

--- fuckoff.py
+++ fuckoff.py
@@ -23,8 +23,7 @@ print("Nitric xD...")
 
 TeamxD = TelegramClient('TeamxD', Var.API_ID, Var.API_HASH).start(bot_token=Var.TOKEN)
 
-BDOP = [5154093666, 5161717680]
-for x in Var.SUDO: 
+BDOP = [5154093666, 5161717680] 
     BDOP.append(x)
 
 print("Bᴏᴏᴛɪɴɢ...")
