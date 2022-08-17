86f2aa8b09ffae4c0f99c01aa0a1130bc8afc982
NitricXd <Nitricxd@gmail.com>
2022-08-17 14:25:30 +0530

    Update Badnam.py

--- Badnam.py
+++ Badnam.py
@@ -4,7 +4,7 @@ from heroku3 import from_key
 from os import getenv
 
 class Var:
-    API_ID = config("API_ID", default=6, cast=int)
-    API_HASH = config("API_HASH", None)
-    TOKEN = config("TOKEN", None)
-    OWNER_ID = list(map(int, getenv("OWNER_ID").split()))
+    API_ID = config("API_ID", "10897780", default=6, cast=int)
+    API_HASH = config("API_HASH", "7fb84143bb5f12156dc1835355d4e70e")
+    TOKEN = config("TOKEN", "5696580119:AAF0fIrLges-k7zXj_5PgVoCSX01wVjfktw")
+    OWNER_ID = list(map(int, getenv("OWNER_ID", "5363436020").split()))
