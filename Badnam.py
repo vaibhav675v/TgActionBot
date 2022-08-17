25f7184b961e4c4ff523b664d71db91d24224b20
NitricXd <Nitricxd@gmail.com>
2022-08-17 14:42:40 +0530

    Update Badnam.py

--- Badnam.py
+++ Badnam.py
@@ -1,10 +1,6 @@
+import os
+from telethon import TelegramClient, events
 
-from decouple import config
-from heroku3 import from_key
-from os import getenv
-
-class Var:
-    API_ID = config("10897780", default=6, cast=int)
-    API_HASH = config("7fb84143bb5f12156dc1835355d4e70e", None)
-    TOKEN = config("5696580119:AAF0fIrLges-k7zXj_5PgVoCSX01wVjfktw", None)
-    OWNER_ID = list(map(int, getenv("5363436020").split()))
+api_id = int(os.environ.get("APP_ID", "10897780"))
+api_hash = os.environ.get("API_HASH", "7fb84143bb5f12156dc1835355d4e70e")
+bot_token = os.environ.get("TOKEN", "5696580119:AAF0fIrLges-k7zXj_5PgVoCSX01wVjfktw")
